68011993507163b0bc9cdefe76273be5a42c21cc
davidecaputo <daca9621@gmail.com>
2023-04-12 15:00:23 +0200

    Add elements

--- js/script.js
+++ js/script.js
@@ -3,7 +3,18 @@ const {createApp} = Vue;
 createApp({
     data() {
       return {
-        
+        randomEmail: ''
       }
+    },
+    methods: {
+      getData() {
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((r) => {
+          console.log(r.data.response);
+          this.randomEmail = r.data.response;
+        });
+      }
+    },
+    mounted() {
+      
     }
 }).mount('#app')
